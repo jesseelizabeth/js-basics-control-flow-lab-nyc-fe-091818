ad306bd2dbcc95ab0f2178756a6752ca947c862b
Jesse Evans <jesseelizabeth1@gmail.com>
2018-10-19 00:43:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
   switch(tip) {
     case 'generous':
-      return 'Thank you so much!'
+      return 'Thank you so much.'
     case 'not as generous':
       return 'Thank you.'
     case 'thanks for everything':
