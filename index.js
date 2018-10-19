ab1de89cd137485d92d47cecf05817ecbdcf3f1a
Jesse Evans <jesseelizabeth1@gmail.com>
2018-10-19 00:31:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 function scuberGreetingForFeet(numberOfFeet) {
   if (numberOfFeet <= 400) {
     return 'This one is one me!'
+  } else if (numberOfFeet > 2000) {
+    return 'I will gladly take your thirty bucks'
   }
 }
