876c6ab62a4f99f302ad4b5f0b383d213542b328
Jesse Evans <jesseelizabeth1@gmail.com>
2018-10-19 00:46:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 function scuberGreetingForFeet(numberOfFeet) {
   if (numberOfFeet <= 400) {
-    return 'This one is one me!'
+    return 'This one is on me!'
   } else if (numberOfFeet > 2000 && numberOfFeet < 2500) {
     return 'I will gladly take your thirty bucks'
   } else if (numberOfFeet > 2500) {
